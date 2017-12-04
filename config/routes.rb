20f483b9c61584682f910117f20f22e64617d46d
Dan Danilyuk and Michael Brooks <michael@>
2017-12-04 10:20:48 -0800

    add devise and adming gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
   resources :order_items
   resources :orders
   resources :products
   resources :accounts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
